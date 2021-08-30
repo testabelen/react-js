0aa3829e2a1cc580f08a8f7fcc7ed1f9ab1e8be2
testabelen <testabelen@gmail.com>
2021-08-30 19:06:32 -0300

    segunda entrega

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar (){
+    return(
+        <div>
+            <h1>CARDENO VINOS</h1>
+            <p>Vinos</p>
+            <p>Cervezas</p>
+            <p>Espumantes</p>
+            <p>Destilados</p>
+            <p>Variete</p>
+        </div>
+    )
+}
+
+export default NavBar
