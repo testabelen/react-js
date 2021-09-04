08a041a4464c722ffe9f9087487eb51efcc48c85
testabelen <testabelen@gmail.com>
2021-09-04 16:13:07 -0300

    error

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,7 @@
+const CartWidget = () => {
+    <>
+        <button><img src={`https://img.flaticon.com/icons/png/512/34/34627.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF`} alt= "Carrito de compras" /></button>
+    </>
+}
+
+export default CartWidget
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,19 @@
+import CartWidget from "./CartWidget"
+
+
 function NavBar (){
     return(
+        <>
         <div>
             <h1>CARDENO VINOS</h1>
-            <p>Vinos</p>
-            <p>Cervezas</p>
-            <p>Espumantes</p>
-            <p>Destilados</p>
-            <p>Variete</p>
+            <a>Vinos</a>
+            <a>Cervezas</a>
+            <a>Espumantes</a>
+            <a>Destilados</a>
+            <a>Variete</a>
         </div>
+        <CartWidget />
+        </>
     )
 }
 
